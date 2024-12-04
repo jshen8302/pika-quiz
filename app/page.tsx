ffcb88f0a728dd5e3aa1c73f5b98b43978f87e64
James Shen <hummus@Jamess-MacBook-Pro.local>
2024-12-03 20:07:51 -0500

    added mobile responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,12 @@ import Image from 'next/image';
 
 export default function Page() {
   return (
-    <main className="flex flex-1 flex-col justify-evenly pb-24 max-w-3xl m-auto">
-      <div className="flex justify-center items-center text-8xl">
+    <main className="flex flex-1 flex-col justify-center gap-10 max-w-sm md:max-w-3xl m-auto">
+      <div className="flex justify-center items-center text-5xl md:text-7xl">
         Pika-Clone Quiz
 
       </div>
-      <div className="flex justify-center items-center text-5xl">
+      <div className="flex justify-center items-center text-2xl md:text-4xl">
         Which Pika-Clone are you?
 
       </div>
@@ -18,13 +18,13 @@ export default function Page() {
         src="/pika-gang.png"
         width={677}
         height={198}
-        className={`${styles.heroImage}`} 
+        className={"w-3/4 md:w-4/5"} 
         alt="Screenshots of the dashboard project showing desktop version"
       />
 
 
       </div>
-      <div className="flex justify-center text-5xl">
+      <div className="flex justify-center text-2xl md:text-4xl">
         start
 
       </div>
--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -5,20 +5,20 @@ import Link from 'next/link'
 
 export default function Navbar() {
   return (
-    <nav className="flex bg-navColor w-full h-20 px-6 text-black">
+    <nav className="flex bg-navColor w-full min-h-20 md:min-h-20 px-6 text-black">
       <div className="flex flex-1  items-center">
         <Image
           src="/binary.jpeg"
           width={50}
           height={50}
-          className={` object-cover rounded-full`}
+          className={`object-cover rounded-full`}
           alt="Screenshots of the dashboard project showing desktop version"
         />
         
 
       </div>
-      <div className="flex flex-1 items-center justify-evenly text-3xl">
-        <Link href="/">
+      <div className="flex flex-1 items-center justify-center gap-10 md:gap-20 text-2xl md:text-3xl">
+        <Link className="" href="/">
           Home
         </Link>
         <div>
